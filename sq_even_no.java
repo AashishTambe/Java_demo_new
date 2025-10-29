8da5e681cc565989d5fbd35b2981655b697296f7
Aashish Tambe <147386333+AashishTambe@users.noreply.github.com>
2025-10-29 18:58:47 +0530

    Add program to print squares of even numbers

---
+++ sq_even_no.java
@@ -0,0 +1,11 @@
+package work;
+
+public class sq_even_no {
+	public static void main(String[] args) {
+		for(int i=300;i<=450;i++) {
+			if(i%2==0) {
+				System.out.println(i*i);
+			}
+		}
+	}
+}
